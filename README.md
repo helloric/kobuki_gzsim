532f49dc27408b0d42758b74a752b5cbc8114024
Andreas Bresser <self@andreasbresser.de>
2025-03-15 22:05:33 +0100

    Initial simple environment and launch file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kobuki Gazebo simulation
 ROS-package for the kobuki base using a current version of the Gazebo simulation
+
+based on https://github.com/gazebosim/ros_gz_project_template
---
+++ launch/kobuki_sim.launch.py
@@ -0,0 +1,90 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+# TODO: namespace-kobuki
+
+
+def generate_launch_description():
+    """Configure ROS nodes for launch."""
+    # Setup project paths
+    pkg_kobuki_desc = get_package_share_directory('kobuki_description')
+    pkg_kobuki_gzsim = get_package_share_directory('kobuki_gzsim')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    # Set gz-sim resource path
+    gz_resource_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH', value=[
+            os.path.join(pkg_kobuki_desc, os.pardir), ':',
+            os.path.join(pkg_kobuki_gzsim, "models")
+        ])
+
+    # Setup to launch the simulator and Gazebo world
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={
+            'gz_args': [
+                '-r -v4 ',
+                PathJoinSubstitution(
+                    [
+                        pkg_kobuki_gzsim,
+                        'worlds',
+                        'simple_room.world'])],
+            'on_exit_shutdown': 'true'
+        }.items(),
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-name', 'kobuki',
+            '-topic', '/robot_description',
+            '-x', '0',
+            '-y', '0',
+            '-z', '0.01'
+        ]
+    )
+
+    bridge_path = os.path.join(
+        pkg_kobuki_gzsim, 'config', 'ros_gz_bridge.yaml')
+    # Bridge ROS topics and Gazebo messages for establishing communication
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[
+            {'config_file': bridge_path},
+            {'qos_overrides./tf_static.publisher.durability':
+                'transient_local'}
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gz_resource_path,
+        gz_sim,
+        bridge,
+        spawn_robot,
+    ])
